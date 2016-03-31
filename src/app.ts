32ea91e9f3c1e6c2a90ec513065eff104b310319
Tyler Rockwood <rockwotj@gmail.com>
2016-03-30 23:06:25 -0400

    Initial commit

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,10 @@
 import {bootstrap} from 'angular2/platform/browser';
 import {MovieQuotesApp} from './app/movie-quotes';
+import {ROUTER_PROVIDERS} from 'angular2/router';
+import {FIREBASE_PROVIDERS, defaultFirebase, AngularFire} from 'angularfire2';
 
-bootstrap(MovieQuotesApp, []);
+bootstrap(MovieQuotesApp, [
+  FIREBASE_PROVIDERS,
+  defaultFirebase("https://rockwotj-moviequotes.firebaseio.com/"),
+  ROUTER_PROVIDERS
+]);
--- src/app/movie-quotes.ts
+++ src/app/movie-quotes.ts
@@ -1,6 +1,8 @@
 import {Component} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
 import {CliRouteConfig} from './route-config';
+import {AngularFire} from 'angularfire2';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
   selector: 'movie-quotes-app',
@@ -15,6 +17,11 @@ import {CliRouteConfig} from './route-config';
 
 export class MovieQuotesApp {
   defaultMeaning: number = 42;
+  moviequotes: Observable<any[]>;
+
+  constructor(af: AngularFire) {
+    this.moviequotes = af.list('/quotes');
+  }
 
   meaningOfLife(meaning?: number) {
     return `The meaning of life is ${meaning || this.defaultMeaning}`;
